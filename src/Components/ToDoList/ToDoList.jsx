be8a2e26f437c5a5888c9d85745a2f1bd1e1ce52
test <test>
2019-07-19 15:35:58 +0300

    todolist

--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, {Component} from "react";
 import './ToDoList.scss'
+import TodoItem from "../ToDoItem/ToDoItem";
+import todosData from "../../Data/TodosData"
 
-function ToDoList (props) {
+class ToDoList extends Component {
 
-  return (
-    <div>
-      Hello
-    </div>
-  )
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData,
+      character: {}
+    }
+  }
+
+  onChangeCompleted = (id) => {
+    this.setState(prevState => {
+      const updateTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+
+      return {
+        todos: updateTodos
+      }
+    })
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.co/api/people/1")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          character: data
+        })
+      })
+  }
+
+  render() {
+    const TodoItems = todosData.map((item) => {
+      return <TodoItem key={item.id} item={item}  onChangeCompleted={this.onChangeCompleted}/>
+    })
+    return (
+      <div className="todo-list">
+        {TodoItems}
+        <div>
+          {this.state.character.name}
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default ToDoList
